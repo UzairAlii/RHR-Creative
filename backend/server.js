f4cb9549b7d5672226bd2bf2b86832bf9d39bbc1
UzairAlii <uzairali89347@gmail.com>
2025-07-24 22:28:04 +0500

    some major changes in backend server.js

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,23 @@ connectCloudinary()
 // middlewares
 
 app.use(express.json())
-app.use(cors({
-  origin: ['https://rhrcreative.com', 'https://admin.rhrcreative.com'],
-  credentials: true
-}))
 
+const allowedOrigins = [
+  'https://rhrcreative.com',
+  'https://admin.rhrcreative.com',
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
 
 
 app.use("/api/user", userRouter);
